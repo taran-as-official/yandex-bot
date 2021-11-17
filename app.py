53a36931b8fa0ea6f7e12a86aef7e3bcacf1ae20
taran-as-official <best.manual.info@gmail.com>
2021-11-17 14:46:54 +0300

    Инициализация проекта

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, json, request
+import logging
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route("/")
+async def main():
+    logging.info(request.json)
+
+    response = {
+        "version": request.json["version"],
+        "session": request.json["session"],
+        "response": {
+            "end_session": False
+        }
+    }
+    response["response"]["text"] = "Привет"
+
+    return json.dumps(response)
+
