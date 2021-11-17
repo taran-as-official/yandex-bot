1c008c1fd3d1f08442d4f20515a24a706a388e5d
taran-as-official <best.manual.info@gmail.com>
2021-11-18 00:40:18 +0300

    Подправлен app

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ app = Flask(__name__)
 
 @app.route('/echo', methods=['POST'])
 def index():
-    logging.info("Запрос от Алисы: " + request.json)
-    print("Запрос от Алисы (print): " +request.json)
+    logging.info("Запрос от Алисы: " + str(request.json))
 
     response = {
         
@@ -26,7 +25,4 @@ def index():
     logging.info(request.json)
     return json.dumps(response)
 
-if __name__ == '__main__':
-    logging.info("Зашли в app.run")
-    app.run(threaded=True)
-    
+
