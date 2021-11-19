afa7d503d9b842789a4791df36a19e49aad4a1ea
taran-as-official <best.manual.info@gmail.com>
2021-11-19 18:09:30 +0300

    Подправлен app

--- app.py
+++ app.py
@@ -2,27 +2,24 @@ from flask import Flask, json, request
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-logging.info("Зашли в app.py")
 
 app = Flask(__name__)
 
 
-
-
 @app.route('/echo', methods=['POST'])
 def index():
-    logging.info("Запрос от Алисы: " + str(request.json))
 
     response = {
         
         "session": request.json["session"],
         "response": {
-            "end_session": False
+            "end_session": False,
+            "text": request.json["request"]["original_utterance"] 		
         },
         "version": request.json["version"]
+
     }
-    response["response"]["text"] = 'Привет'
-    logging.info(request.json)
+
     return json.dumps(response)
 
 
